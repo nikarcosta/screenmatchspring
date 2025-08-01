5b8cd50f30935ca221cd3b4e38e2036eff3dca18
nikarcosta <mrc32467098@outlook.com>
2025-08-01 18:50:27 -0300

    initial commit

---
+++ src/main/java/com/nika/screenmatchspring/ScreenmatchspringApplication.java
@@ -0,0 +1,28 @@
+package com.nika.screenmatchspring;
+
+import com.nika.screenmatchspring.model.DadosSerie;
+import com.nika.screenmatchspring.service.ConsumoAPI;
+import com.nika.screenmatchspring.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchspringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchspringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		String apiKey = "";
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=" + apiKey);
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
