c90ce75b0759ce819e1c2b8195391c8f3c0046ab
nikarcosta <mrc32467098@outlook.com>
2025-08-09 13:52:15 -0300

    feat: add DadosEpisodio model

--- src/main/java/com/nika/screenmatchspring/ScreenmatchspringApplication.java
+++ src/main/java/com/nika/screenmatchspring/ScreenmatchspringApplication.java
@@ -1,5 +1,6 @@
 package com.nika.screenmatchspring;
 
+import com.nika.screenmatchspring.model.DadosEpisodio;
 import com.nika.screenmatchspring.model.DadosSerie;
 import com.nika.screenmatchspring.service.ConsumoAPI;
 import com.nika.screenmatchspring.service.ConverteDados;
@@ -22,7 +23,13 @@ public class ScreenmatchspringApplication implements CommandLineRunner {
 		System.out.println(json);
 
 		ConverteDados conversor = new ConverteDados();
+
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=" + apiKey);
+
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
