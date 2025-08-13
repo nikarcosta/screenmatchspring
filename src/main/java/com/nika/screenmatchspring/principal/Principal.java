72673bb45acd3176bf970df25edf1f48019dc6d1
nikarcosta <mrc32467098@outlook.com>
2025-08-13 18:05:56 -0300

    refactor: refactor logic to display episodes's titles

--- src/main/java/com/nika/screenmatchspring/principal/Principal.java
+++ src/main/java/com/nika/screenmatchspring/principal/Principal.java
@@ -39,11 +39,6 @@ public class Principal {
 
 		temporadas.forEach(System.out::println);
 
-        for(int i = 0; i < dados.totalTemporadas(); i++){
-            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-            for(int j = 0; j < episodiosTemporada.size(); j++){
-                System.out.println(episodiosTemporada.get(j).titulo());
-            }
-        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
