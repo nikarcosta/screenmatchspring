1e7d2dc34043d374b8555285cd7f10fdd7192f2b
nikarcosta <mrc32467098@outlook.com>
2025-08-15 08:14:03 -0300

    feat: add Episodio model

--- src/main/java/com/nika/screenmatchspring/principal/Principal.java
+++ src/main/java/com/nika/screenmatchspring/principal/Principal.java
@@ -3,12 +3,15 @@ package com.nika.screenmatchspring.principal;
 import com.nika.screenmatchspring.model.DadosEpisodio;
 import com.nika.screenmatchspring.model.DadosSerie;
 import com.nika.screenmatchspring.model.DadosTemporada;
+import com.nika.screenmatchspring.model.Episodio;
 import com.nika.screenmatchspring.service.ConsumoAPI;
 import com.nika.screenmatchspring.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -37,8 +40,24 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 
-		temporadas.forEach(System.out::println);
-
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\n Top 5 episódios:");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
     }
 }
